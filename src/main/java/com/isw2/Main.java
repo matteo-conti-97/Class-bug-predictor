a25453dd08ec265e876d10d2776301143358ba48
Matteo Conti <56634851+matteo-conti-97@users.noreply.github.com>
2023-04-06 23:46:12 +0200

    #2 - resolved bugs with jdk

--- src/main/java/com/isw2/Main.java
+++ src/main/java/com/isw2/Main.java
@@ -1,5 +1,11 @@
 package com.isw2;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String REPO_PATH = "C:\\Users\\matte\\Documents\\GitHub\\isw2-jira-git-measurement";
+        String FILTER = "added";
+        GitFilter gitFilter = new GitFilter(REPO_PATH);
+        System.out.println(gitFilter.filterLog(FILTER));
+    }
 }
