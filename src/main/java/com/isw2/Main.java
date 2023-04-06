bfaafc8a53a0c3c3637b1b0442a5f0e0b59eb3ea
Matteo Conti <56634851+matteo-conti-97@users.noreply.github.com>
2023-04-07 00:07:24 +0200

    #5 - refactor sonar code smell

--- src/main/java/com/isw2/Main.java
+++ src/main/java/com/isw2/Main.java
@@ -2,9 +2,7 @@ package com.isw2;
 
 public class Main {
     public static void main(String[] args) {
-        String repoPath = "C:\\Users\\matte\\Documents\\GitHub\\isw2-jira-git-measurement";
-        String filter = "#";
-        GitFilter gitFilter = new GitFilter(repoPath);
-        System.out.println(gitFilter.filterLog(filter));
+        GitFilter gitFilter = new GitFilter(Constants.REPO_PATH);
+        System.out.println(gitFilter.filterLog(Constants.FILTER));
     }
 }
